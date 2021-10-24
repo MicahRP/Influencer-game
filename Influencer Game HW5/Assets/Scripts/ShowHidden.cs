d7e5b391c26c7c6215bd951ad425f3d7826b6cdc
emmelinee <emmeline.meyers@tufts.edu>
2021-10-24 19:17:58 -0400

    made hideobject script

--- Influencer Game HW5/Assets/Scripts/ShowHidden.cs
+++ Influencer Game HW5/Assets/Scripts/ShowHidden.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HideOrReveal : MonoBehaviour
+//object starts hidden and you can reveal later on then hide again + repeat
+public class ShowHidden : MonoBehaviour
 {	
+	public GameObject gameobject;
 	
-	public GameObject gameobject; 
-	
+	void Start(){
+	gameobject.SetActive(false);
+	}
+
 	public void HideObject()
 		{
 			gameobject.SetActive(false);		
